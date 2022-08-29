fa719a89715cb75bfc36c7f0c2aaef0b5d29932c
Vinicius <vini.fal2002@gmail.com>
2022-08-29 20:52:30 -0300

    Simple proportional control

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #define STBY 2
 #define PWMA 5
 
+int setpoint = 512;
+int error;
+
 void setup() {
 
   Serial.begin(9600);
@@ -14,48 +17,24 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(PWMA, OUTPUT);
 
-
   digitalWrite(STBY, HIGH);
-  // digitalWrite(3, HIGH);
-  // digitalWrite(4, LOW);
-  // analogWrite(PWMA, 100);
-  // delay(1000);
-  // digitalWrite(4, HIGH);
+
+
 }
 
 void loop() {
-  digitalWrite(3, HIGH);
-  digitalWrite(4, LOW);
-  for (int i = 0; i < 256; i++){
-    Serial.println(analogRead(A0));
-    analogWrite(PWMA, i);
-    delay(10);
+  int leitura = analogRead(A0);
+  Serial.println(leitura);
+  error = leitura - setpoint;
+
+  if (error > 0){
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }else
+  {
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
   }
-  delay(100);
-  for (int i = 255; i >= 0; i--){
-    Serial.println(analogRead(A0));
-    analogWrite(PWMA, i);
-    delay(10);
-  }
-  digitalWrite(4, HIGH);
-  digitalWrite(3, LOW);
-  for (int i = 0; i < 256; i++){
-    Serial.println(analogRead(A0));
-    analogWrite(PWMA, i);
-    delay(10);
-  }
-  delay(100);
-  for (int i = 255; i >= 0; i--){
-    Serial.println(analogRead(A0));
-    analogWrite(PWMA, i);
-    delay(10);
-  }
-  // delay(2000);
-  // digitalWrite(3, LOW);
-  // digitalWrite(4, HIGH);
-  // digitalWrite(13, LOW);
-  // delay(2000);
-  // digitalWrite(3, HIGH);
-  // digitalWrite(4, LOW);
-  // digitalWrite(13, HIGH);
+  analogWrite(PWMA, error/10);
+  
 }
