5a7a63eb0c8475052c2d3dd47d41c6f0f136f233
Vinicius <vini.fal2002@gmail.com>
2022-09-12 19:06:03 -0300

    Create buffer

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #define STBY 2
 #define PWMA 5
 
-int setpoint = 512;
-int error;
+#define N 256
+
+uint16_t setpoint = 512;
+int16_t erro[N];
+uint16_t leitura[N];
+uint8_t comando[N];
+uint8_t direction = 0;
 
 void setup() {
 
@@ -19,22 +24,24 @@ void setup() {
 
   digitalWrite(STBY, HIGH);
 
+  for (uint8_t i = 0; i < 256; i++)
+  {
+    leitura[i] = analogRead(A0);
+    erro[i] = leitura[i] - setpoint;
 
+    if (erro[i] > 0){
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+    }else
+    {
+      digitalWrite(3, LOW);
+      digitalWrite(4, HIGH);
+    }
+    analogWrite(PWMA, erro[i] >> 3);
+    delay(10);
+  }
+  Serial.println(leitura[0]);
 }
 
 void loop() {
-  int leitura = analogRead(A0);
-  Serial.println(leitura);
-  error = leitura - setpoint;
-
-  if (error > 0){
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-  }else
-  {
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-  }
-  analogWrite(PWMA, error/10);
-  
 }
