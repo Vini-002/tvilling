7b7a50336af1bd721aadb26298355a1b6f8c0a06
Vinicius <vini.fal2002@gmail.com>
2023-05-25 18:26:01 -0300

    Add pinout table on separate file

---
+++ include/pinout.h
@@ -0,0 +1,24 @@
+// =============== MOTOR DIGITAL INPUT ===============
+
+// #define AIN1 
+// #define AIN2 
+#define BIN1 26
+#define BIN2 21
+#define CIN1 5
+#define CIN2 17
+#define DIN1 16
+#define DIN2 4
+
+// ================= MOTOR PWM INPUT =================
+
+#define PWMA 23 
+#define PWMB 19
+#define PWMC 18
+#define PWMD 15
+
+// ================= MOTOR POSITION OUTPUT =================
+
+#define POS_A 36
+#define POS_B 39
+#define POS_C 34
+#define POS_D 35
